2f9c223f9a808ddb6c5e1ec0cc5bae0e20f89fc9
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-04-20 01:43:51 -0300

    Add api address

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://imc-hiring-test.azurewebsites.net/Contact',
   timeout: 8000,
 });
 
